358f2a2a5a68d5ad50ee6d9b26f3318f3044cdcb
assayagt <assayagt@post.bgu.ac.il>
2024-04-18 13:53:52 +0300

    Added game managment to server

--- Server.py
+++ Server.py
@@ -14,7 +14,8 @@ MESSAGE_TYPE = 0x2
 SERVER_PORT = 12345
 SERVER_NAME = "TeamMysticServer".ljust(32, '\0')  # Make the server name 32 characters long
 clientHandlers = []
-POSSIBLE_ANSWERS = ['Y', 'T', '1', 'N', 'F', '0']
+POSSIBLE_TRUE = ['Y', 'T', '1']
+POSSIBLE_FALSE = ['N', 'F', '0']
 # Event to signal the end of waiting time
 waiting_time_over = threading.Event()
 
@@ -86,6 +87,95 @@ def acceptClients(tcpSocket):
         clientThread.start()
         clientHandlers.append(clientHandler)
 
+def getRandomQuestion():
+    # Choose one random trivia question
+    random_question = random.choice(trivia_questions)
+    random_question_to_send = random_question["question"]
+    random_question_to_send = f"True or false: {random_question_to_send}"
+    return random_question, random_question_to_send
+
+def manageGame():
+    if len(clientHandlers) < 1:
+        print("No clients connected to this trivia...")
+        return
+    player_names = ["Player" + str(index + 1) + ": " + client.getPlayerName() + "\n" for index, client in enumerate(clientHandlers)]
+    welcome_message = f"Welcome to the Mystic server, where we are answering trivia questions about Aston Villa FC.\n {' '.join(player_names)}"
+    
+    for client in clientHandlers:
+        try:
+            client.sendInfoToClient(welcome_message)
+        except:
+            break
+    print(welcome_message)
+    
+    winner_found = False
+    while not winner_found:
+        random_question, random_question_to_print = getRandomQuestion()
+        for client in clientHandlers:
+            try:
+                client.sendInfoToClient(random_question_to_print)
+            except:
+                break
+            client.startGame()
+        print(random_question_to_print)
+
+        time.sleep(5)
+
+        
+        winner_name = ""
+
+        sem = threading.Semaphore(0)
+        sem.acquire()
+
+        # Receive and check answers from clients
+        response = clientHandlers[0].getAnswer()
+
+        if response is not None: #Can be removed when notify added
+            # Check if the answer is correct
+            if response in POSSIBLE_TRUE and trivia_questions[trivia_questions.index(random_question)]['is_true']:
+                winner_name = client.getPlayerName()
+                print(f"\n{winner_name} is correct! {winner_name} wins!")
+                winner_found = True
+                # Broadcast message to all clients about the winner
+                winner_message = f"\n{winner_name} is correct! {winner_name} wins!"
+                for other_client in clientHandlers:
+                    try:
+                        other_client.endGame()
+                        other_client.sendInfoToClient(winner_message)
+                    except:
+                        continue
+            elif response in POSSIBLE_FALSE and not trivia_questions[trivia_questions.index(random_question)]['is_true']:
+                winner_name = client.getPlayerName()
+                print(f"\n{winner_name} is correct! {winner_name} wins!")
+                winner_found = True
+                # Broadcast message to all clients about the winner
+                winner_message = f"\n{winner_name} is correct! {client.getPlayerName()} wins!"
+                for other_client in clientHandlers:
+                    try:
+                        other_client.endGame()
+                        other_client.sendInfoToClient(winner_message)
+                    except:
+                        continue
+            else:
+                print(f"Incorrect answer from {client.getPlayerName()}. They are disqualified.")
+        
+
+
+        # Need to fix that so there will be a new question
+        if not winner_found:
+            end_message = "\nNo correct answers were received, so there will be another question."
+        else:
+            end_message = f"Game over!\nCongratulations to the winner: {winner_name}"
+
+        for client in clientHandlers:
+            try:
+                client.resetAnswer()
+                client.sendInfoToClient(end_message)
+            except:
+                continue
+        time.sleep(1)
+
+
 def Main():
     udpSocekt = initUDP()
     while True:
@@ -93,6 +183,7 @@ def Main():
         acceptClientsThread = threading.Thread(target=acceptClients, args=(tcpSocket,))
         acceptClientsThread.start()
         waitForClients(tcpSocket, udpSocekt)
+        manageGame()
         print("Game over, sending out offer requests...")
 
 
